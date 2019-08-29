69887dba5bce0f21e31e86b8f2fdb58915e67de5
Ignacio Marzotta <marzottanacho@gmail.com>
2019-08-29 12:16:55 -0400

    Added Bootstrap's CDN and set 'one' as main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/one'
   get 'pages/two'
+  #Uses 'root' to define the main page (localhost:3000)
+  root 'pages#one'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
